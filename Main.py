5292c8180b004059c84c6677bb4dff8f6d4c5969
brettford1999 <brettford1118@gmail.com>
2023-10-10 00:13:34 -0500

    added functions and testing remote connection

--- Main.py
+++ Main.py
@@ -1,12 +1,59 @@
 #let's create a tool to help with helpdesk work
-#The first thing I'd like to do is pull the system information from a windows computer
-
 import wmi
-import ctypes
+import os
+import subprocess
+from socket import *
 
-wmi_block = wmi.WMI()    
-systeminfo = wmi_block.Win32_ComputerSystem()[0]
+# I am using c because that is what is used in the wmi documentation
+# I may rewrite this later to be more descriptive
+c = wmi.WMI()
 
-#Manufacturer = systeminfo.Manufacturer
-#Model = systeminfo.Model
-print(systeminfo)
+def get_sysinfo():
+    systeminfo = c.Win32_ComputerSystem()[0]
+
+def get_startup(): 
+    for s in c.Win32_StartupCommand ():
+        print(f"{s.Location} - {s.Caption} - {s.Command}")
+
+#the watcher function is going to need troubleshooting
+def event_log_watcher():
+    watcher = c.watch_for (
+    notification_type="Creation",
+    wmi_class="Win32_NTLogEvent",
+    Type="error"
+    )
+    while 1:
+        error = watcher ()
+        print(f"Error in {error.Logfile} log: {error.Message}")
+
+# I may just create a subprocess function and just have a convenient menu to run powershell commands
+# when the tool is open
+def gpupdate():
+    subprocess.run(["powershell", "gpupdate /force"])
+
+
+
+#print(get_startup())
+
+
+
+ip = '192.168.160.1'
+username = '********'
+password = '********'
+
+
+import winrm
+
+# Create winrm connection.
+sess = winrm.Session(f'https://{ip}', auth=(f'{username}', f'{password}'), transport='kerberos')
+result = sess.run_cmd('ipconfig', ['/all'])
+
+
+"""
+try:
+    print(f"Establishing connection to {ip}")
+    connection = wmi.WMI(ip, user=username, password=password)
+    print("Connection established")
+except wmi.x_wmi:
+    print(f"Your Username and Password of {getfqdn(ip)} are wrong.")
+"""
--- README.md
+++ README.md
@@ -3,4 +3,23 @@ The goal of this project is to create a suite of tools to assist in the daily wo
 requirements:
     python 3.10.12
     pip install WMI
-    sudo apt-get install libffi-dev
+    pip install pywinrm requests_kerberos
+    pip install pykerberos
+    https://visualstudio.microsoft.com/visual-cpp-build-tools/
+
+
+Plans:
+    Create useful functions that can gather information or perform tasks
+    Create a GUI to make the functions more accessible
+
+
+Resources:
+    http://timgolden.me.uk/python/wmi/cookbook.html
+    https://pypi.org/project/WMI/
+    https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-computersystem
+    https://stackoverflow.com/questions/18961213/how-to-connect-to-a-remote-windows-machine-to-execute-commands-using-python
+
+
+#I'm leaving off troubleshooting kerberos 
+# C:\Users\brett\AppData\Local\Temp\pip-install-tn7q94vk\pykerberos_20c8ac485f2d4faab3a18996df5c5b6e\src\kerberosbasic.h(17): fatal error C1083: Cannot open include file: 'gssapi/gssapi.h': No such file or directory
+      error: command 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Tools\\MSVC\\14.37.32822\\bin\\HostX86\\x86\\cl.exe' failed with exit code 2
